5cbcc2180a835ba40a8a2407c8183f7872266a2d
Tyyni Repo <331213902+TyyniRepo@users.noreply.github.com>
2026-09-22 20:43:26 +0300

    Osa 1 tehtävät 1.3-1.5

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,11 +1,11 @@
 /**
- * @param {{course: string}} props
+ * @param {{name: string}} props
  * @returns {React.JSX.Element}
  */
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.name}</h1>
     </>
   )
 }
@@ -25,52 +25,52 @@ const Part = (props) => {
 }
 
 /**
- * @param {{part1: string, exercises1: number, part2: string, exercises2: number, part3: string, exercises3: number}} props 
+ * @param {{name: string, parts: Array.<{name: string, exercises: number}>}} props 
  * @returns {React.JSX.Element}
  */
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   )
 }
 
 /**
- * @param {{exercises1: number, exercises2: number, exercises3: number}} props 
+ * @param {{parts: Array.<{name: string, exercises: number}>}} props  
  * @returns {React.JSX.Element}
  */
 const Total = (props) => {
+  const sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {sum}</p>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    }, {
+      name: 'Using props to pass data',
+      exercises: 7
+    }, {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
